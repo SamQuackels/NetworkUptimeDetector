b7cf8852c7c0a25b425cfa91fb56bc4ea34137a0
Sam Quackels <sam.quackels@student.hogent.be>
2024-05-16 02:00:34 +0200

    Initial and possibly final commit

---
+++ NetworkUptime.py
@@ -0,0 +1,70 @@
+import os
+import subprocess
+from datetime import datetime
+import time
+import signal
+from time import gmtime
+from subprocess import DEVNULL
+
+directory = "/NetworkUptimeDetector"
+directoryFile = "/NetworkUptimeDetector/log.txt"
+
+
+ended = False
+
+
+# Make folder if possible
+if not os.path.exists(directory):
+    # Create the directory
+    os.makedirs(directory)
+    print("Directory created successfully!")
+else:
+    print("Found directory!")
+
+# Make file if possible
+if not os.path.isfile(directoryFile):
+    #Create the txt file
+    f=open(directoryFile,"w+")
+    f.write("Network Uptime Detector\n")
+    f.write("Author: Sam Quackels\n")
+    f.write("Made on: 16/05/2024\n")
+    f.write("|       Date       |       Time started       |       Time ended       |       Duration       |\n")
+    f.close()
+    print("Log file created successfully!")
+else:
+    print("Found log file!")
+    
+
+# Starting detection
+address = "8.8.8.8"
+ttl = "20"
+command = "ping " + address + " -i " + ttl + " -n 1"
+while not ended:
+    p = subprocess.Popen(command, stdout=DEVNULL)
+    p.wait()
+    code = (p.returncode)
+    # Could not ping
+    if code == 1:
+
+        date = datetime.now().strftime("%d/%m/%Y")
+        startTime = datetime.now().strftime("%H:%M:%S")
+        startTimeObject = datetime.now()
+        
+        print("Lost connection at : " + startTime)
+        while code == 1 and not ended:
+            p = subprocess.Popen(command, stdout=DEVNULL)
+            p.wait()
+            code = (p.returncode)
+            time.sleep(2)
+        stopTime = datetime.now().strftime("%H:%M:%S")
+        stopTimeObject = datetime.now()
+        print("Connection restored at : " + stopTime)
+        durationFallout = int((stopTimeObject - startTimeObject).seconds)
+        durationFallout = time.strftime("%H:%M:%S", gmtime(durationFallout))
+        print("Duration fallout : " + durationFallout)
+        f=open(directoryFile,"a+")
+        stringWrite = "|    {0}    |        {1}          |        {2}        |       {3}       |\n".format(date, startTime, stopTime, durationFallout)
+        f.write(stringWrite)
+        f.close()
+
+    time.sleep(2)
